d5a50b78f769035cc30371c8ff7a7df7d76b3bf7
Olivier LIN-SI-CHENG <olivier.lin-si-cheng@pole-emploi.fr>
2017-12-15 17:27:32 +0100

    add decorator ApiPath and ApiGet

--- src/lib/swagger-specification/decorators/api.decorator.ts
+++
@@ -1,7 +0,0 @@
-export function Api() {
-    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
-        console.log(target);
-        console.log(propertyKey);
-        console.log(descriptor);
-    };
-}
--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -1,15 +1,19 @@
 import * as express from "express";
-import {injectable} from "inversify";
-import {controller, httpGet, interfaces} from "inversify-express-utils";
-import { Api } from "../lib/swagger-specification";
+import { injectable } from "inversify";
+import { controller, httpGet, interfaces } from "inversify-express-utils";
+import { ApiPath, ApiGet } from "../lib/swagger-specification";
 const pkg = require( "../../package.json" );
 
-@Api()
+@ApiPath( "/" )
 @controller( "/" )
 @injectable()
 export class VersionController implements interfaces.Controller {
     public static TARGET_NAME: string = "VersionController";
 
+    @ApiGet({
+        description: "Version object that need to be added",
+        summary: "Add a new Version"
+    })
     @httpGet( "/" )
     public get( request: express.Request, response: express.Response, next: express.NextFunction ): void {
         response.json( {
