11ed04a8f321361fa13caa3655bf31745b9913e8
Olivier LIN-SI-CHENG <olivier.lin-si-cheng@pole-emploi.fr>
2018-04-24 16:10:56 +0200

    add other constants in SwaggerDefinitionConstant

--- src/lib/swagger-express-ts/swagger-definition.constant.ts
+++ src/lib/swagger-express-ts/swagger-definition.constant.ts
@@ -1,6 +1,15 @@
 export class SwaggerDefinitionConstant {
   private static JSON: string = "application/json";
   private static XML: string = "application/xml";
+  private static ZIP: string = "application/zip";
+  private static PDF: string = "application/pdf";
+  private static X_WWW_FORM_URLENCODED: string = "application/x-www-form-urlencoded";
+  private static FORM_DATA: string = "multipart/form-data";
+  private static TEXT_PLAIN: string = "text/plain";
+  private static TEXT_HTML: string = "text/html";
+  private static PNG: string = "image/png";
+  private static GIF: string = "image/gif";
+  private static JPEG: string = "image/jpeg";
   private static STRING: string = "string";
   private static NUMBER: string = "number";
   private static INTEGER: string = "integer";
@@ -9,8 +18,17 @@ export class SwaggerDefinitionConstant {
   private static OBJECT: string = "object";
   private static QUERY: string = "query";
   public static Produce = {
+    JSON: SwaggerDefinitionConstant.JSON,
     XML: SwaggerDefinitionConstant.XML,
-    JSON: SwaggerDefinitionConstant.JSON
+    ZIP: SwaggerDefinitionConstant.ZIP,
+    PDF: SwaggerDefinitionConstant.PDF,
+    X_WWW_FORM_URLENCODED: SwaggerDefinitionConstant.X_WWW_FORM_URLENCODED,
+    FORM_DATA: SwaggerDefinitionConstant.FORM_DATA,
+    TEXT_PLAIN: SwaggerDefinitionConstant.TEXT_PLAIN,
+    TEXT_HTML: SwaggerDefinitionConstant.TEXT_HTML,
+    PNG: SwaggerDefinitionConstant.PNG,
+    GIF: SwaggerDefinitionConstant.GIF,
+    JPEG: SwaggerDefinitionConstant.JPEG
   };
 
   public static Scheme = {
