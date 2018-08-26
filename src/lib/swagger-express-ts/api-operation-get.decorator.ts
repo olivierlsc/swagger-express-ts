fc7266f837e5557838101e259cb8e341d34e45cb
ariel <ariel@compie.co.il>
2018-08-26 13:25:25 +0300

    added support for multiple api versions.

--- src/lib/swagger-express-ts/api-operation-get.decorator.ts
+++ src/lib/swagger-express-ts/api-operation-get.decorator.ts
@@ -1,13 +1,20 @@
-import { SwaggerService } from "./swagger.service";
-import { IApiOperationArgsBase } from "./i-api-operation-args.base";
-export interface IApiOperationGetArgs extends IApiOperationArgsBase {}
+import {SwaggerService} from "./swagger.service";
+import {IApiOperationArgsBase} from "./i-api-operation-args.base";
+
+export interface IApiOperationGetArgs extends IApiOperationArgsBase {
+}
 
 export function ApiOperationGet(args: IApiOperationGetArgs): MethodDecorator {
-  return function(
-    target: any,
-    propertyKey: string | symbol,
-    descriptor: PropertyDescriptor
-  ) {
-    SwaggerService.getInstance().addOperationGet(args, target, propertyKey);
-  };
+    return function (target: any,
+                     propertyKey: string | symbol,
+                     descriptor: PropertyDescriptor) {
+        const apiVersions = args.apiVersion || ["v1"];
+        apiVersions.forEach((apiV: string) => {
+            SwaggerService.getInstance(apiV).addOperationGet(
+                args,
+                target,
+                propertyKey
+            );
+        });
+    };
 }
--- src/lib/swagger-express-ts/express.configurator.ts
+++ src/lib/swagger-express-ts/express.configurator.ts
@@ -1,45 +1,55 @@
-import { Router, Request, Response, NextFunction } from "express";
-import { SwaggerService } from "./swagger.service";
+import {Router, Request, Response, NextFunction} from "express";
+import {SwaggerService} from "./swagger.service";
 import * as assert from "assert";
-import { build, ISwaggerBuildDefinition } from "./swagger.builder";
-import { ISwagger } from "./i-swagger";
+import {build, ISwaggerBuildDefinition} from "./swagger.builder";
+import {ISwagger} from "./i-swagger";
 
 export interface ISwaggerExpressOptions {
-  /**
-   * Path of resource.
-   * Default is "/api-docs/swagger.json".
-   */
-  path?: string;
+    /**
+     * Path of resource.
+     * Default is "/api-docs/swagger.json".
+     */
+    path?: string;
 
-  /**
-   * Swagger Definition.
-   */
-  definition?: ISwaggerBuildDefinition;
+    /**
+     * Swagger Definition.
+     */
+    definition?: ISwaggerBuildDefinition;
+
+    /**
+     * Api Version.
+     * Default is "v1".
+     */
+    apiVersion?: string;
 }
 
 export function express(options?: ISwaggerExpressOptions): Router {
-  let path: string = "/api-docs/swagger.json";
-  if (options) {
-    assert.ok(options.definition, "Definition is required.");
-    if (options.path) {
-      path = options.path;
+    let path: string = "/api-docs/swagger.json";
+    let apiVersion: string = "v1";
+    if (options) {
+        assert.ok(options.definition, "Definition is required.");
+        if (options.path) {
+            path = options.path;
+        }
+        if (options.apiVersion) {
+            apiVersion = options.apiVersion;
+        }
+        if (options.definition) {
+            build(options.definition, apiVersion);
+        }
     }
-    if (options.definition) {
-      build(options.definition);
-    }
-  }
-  const router = buildRouter(path);
-  return router;
+    const router = buildRouter(path, apiVersion);
+    return router;
 }
 
-function buildRouter(path: string): Router {
-  const router: Router = Router();
-  router.get(
-    path,
-    (request: Request, response: Response, next: NextFunction) => {
-      let data: ISwagger = SwaggerService.getInstance().getData();
-      response.json(data);
-    }
-  );
-  return router;
+function buildRouter(path: string, version: string): Router {
+    const router: Router = Router();
+    router.get(
+        path,
+        (request: Request, response: Response, next: NextFunction) => {
+            const data: ISwagger = SwaggerService.getInstance(version).getData();
+            response.json(data);
+        }
+    );
+    return router;
 }
